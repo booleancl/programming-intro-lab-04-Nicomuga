72c4db9f471212dc128604c17ef561e9efe877ac
Muga <nmugasaez@gmail.com>
2022-09-21 11:00:23 -0300

    All together

--- file_handling/all_together.py
+++ file_handling/all_together.py
@@ -1,25 +1,39 @@
+
 print('Bienvenido al programa')
 user_input = ''
 
-while user_input != 'exit':
+def write_file():
+    file = open('file_handling/demo_two.txt', 'a')
+    user_content = input('Ingresa el contenido\n')
+    file.write(user_content + "\n")
+    file.close()
+
+def read_file():
+    file = open('file_handling/demo_two.txt', 'r')
+    for line in file:
+        print(line)
+def print_menu():
     print('########################')
     print('ingresa una opcion \n')
     print('1', 'agregar contenido\n')
     print('2', 'leer contenido\n')
     print('exit', 'Para salir\n')
     print('########################')
+        
+while user_input != 'exit':
+    print_menu()
 
     user_input = input()
 
+
+
     if user_input == '1':
-        file = open('file_handling/demo_two.txt', 'a')
-        user_content = input('Ingresa el contenido\n')
-        file.write(user_content + '\n')
-        file.close()
+        write_file()
     elif user_input == '2':
-        file = open('file_handling/demo_two.txt', 'r')
-        for line in file:
-            print(line)
+        read_file()
+    elif user_input == 'exit':
+        print('Chau Chau')
     else:
         print('opcion incorrecta')
-print('Chau Chau')
+
+
