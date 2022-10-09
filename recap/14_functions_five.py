8fcb105044b1781b2f70d2d30139cd822159f229
Muga <nmugasaez@gmail.com>
2022-10-08 23:43:52 -0300

    problemas funcionando

--- recap/14_functions_five.py
+++ recap/14_functions_five.py
@@ -1,8 +1,8 @@
 # Crear una funcion que imprima los numeros divisibles por 3 hasta el numero n 
 
 def divisibles_by_three(n):
-    if n % 3 == 0:
-        for num in range(n+1):
+    for num in list(range(n+1)):
+        if num % 3 == 0:
             print(num)
 
 divisibles_by_three(50)
@@ -10,7 +10,22 @@ divisibles_by_three(50)
 #Crear una funcion que sume todos los numeros hasta el numero n
 
 def sum_up_to(n):
-    for num in range(n+1):
-        return num + num
+    return sum(range(n+1))
 
 print(sum_up_to(20))
+
+
+# Crear una función que calcule el volumen de una esfera de radio r
+
+def ball_volume(r):
+    return (4/3) * 3.1416 *r**3 
+    
+
+print(ball_volume(15))
+
+# Crear una función que entregue el volumen de un cilindro de radio r y altura h
+
+def cylinder_volume(r,h):
+    return 3.14 * r**2 * h
+
+print(cylinder_volume(10,5))
